a66dcbe3ba0e200cdfaf0279e9d3f1e5a8d58ec4
Kapil Devkota <kd312@dcc-h200-gpu-01.rc.duke.edu>
2025-08-11 19:41:42 -0400

    added processing_data and loader

---
+++ precise/processing_data.py
@@ -0,0 +1,7 @@
+# This function takes in a pdb file without any ligand information.
+# Into this pdb, it attaches the ligand in a random pocket.
+# The output should be a pdb file containing the protein+ligand complex
+# without any steric clashes.
+
+def attach_ligand_validly(pdb_file, ligand_smi, output_pdb_file):
+    pass
